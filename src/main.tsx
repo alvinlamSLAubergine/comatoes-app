3909d7c70ede130e39bd8be3cb9161e3ae04e139
alvinlamAgridence <alvin.lam@agridence.com>
2025-01-25 15:39:23 +0800

    Add basename to router instead of adding homepage URL everywhere

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ROUTES } from './navigation/routes';
 
-const router = createBrowserRouter([ROUTES]);
+const router = createBrowserRouter([ROUTES], {
+  basename: '/comatoes-app',
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -3,30 +3,27 @@ import App from '../App';
 import { ClientDetailsPage, ClientsPage, TestPage2 } from '../pages';
 import { PLAYGROUND_ROUTES } from './playground-routes';
 
-const HOME_URL = '/comatoes-app';
-
 export const ROUTES: RouteObject = {
   Component: App,
-  path: `${HOME_URL}`,
   children: [
     {
-      path: HOME_URL,
-      element: <Navigate to={`${HOME_URL}/clients`} />,
+      path: '/',
+      element: <Navigate to='/clients' />,
     },
     {
-      path: `${HOME_URL}/clients`,
+      path: '/clients',
       Component: ClientsPage,
     },
     {
-      path: `${HOME_URL}/clients/:clientId`,
+      path: '/clients/:clientId',
       Component: ClientDetailsPage,
     },
     {
-      path: `${HOME_URL}/test-page-2`,
+      path: '/test-page-2',
       Component: TestPage2,
     },
     {
-      path: `${HOME_URL}/playgrounds`,
+      path: '/playgrounds',
       children: PLAYGROUND_ROUTES,
     },
   ],
